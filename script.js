bc55c00965133a8677a4be6207c543fb2221d9c9
Yessenia Aguilar <yaguilar@wlapcs.org>
2017-02-16 19:28:09 +0000

    my first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function clearDisplay(){
+    document.getElementById("display-bar").innerHTML = ""
+}
+
+function display(userInput){
+    document.getElementById("display-bar").innerHTML += userInput;
+}
+
+function calculate(){
+   var answer = eval(document.getElementById("display-bar").innerHTML);
+   document.getElementById("display-bar").innerHTML = answer;
+}
